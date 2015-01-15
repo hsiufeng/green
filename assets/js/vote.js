53548abcf8788548d7b90fcfc611f8b39bda9a49
hsiufeng <hsiufeng0401@gmail.com>
2015-01-16 02:18:25 +0800

    NEW0116
    mail&pie

--- assets/js/vote.js
+++ assets/js/vote.js
@@ -27,6 +27,20 @@ function Vote(value) {
     var query = new Parse.Query(school);
 
     query.equalTo("user_mail", user_mail);
+    query.find({
+        success: function(String) {
+             re = /^.+@.+\..{2,3}$/;
+        if (re.test(user_mail)){
+              alert("Email符合，計票中...");
+         }else{
+              alert("Email格式不符，請重新輸入！");
+              
+            }
+        },
+        error: function(error) {
+
+        }
+    });
     query.count({
         success: function(count) {
             if(count>0){  //是否投過票
